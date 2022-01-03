43e84dde913652a8ffe38d82d0a4d5567cdef600
cedcoss <priyvratdwivedi@cedcommerce.com>
2022-01-03 14:29:20 +0530

    first commit

--- app/src/main/java/com/example/myrecyclerdemo/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerdemo/MainActivity.kt
@@ -1,46 +1,56 @@
 package com.example.myrecyclerdemo
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import androidx.core.content.withStyledAttributes
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myrecyclerdemo.NewsInterface.NewsService.newsInstance
-
+import com.littlemango.stacklayoutmanager.StackLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-
 class MainActivity : AppCompatActivity() {
 lateinit var adapter: NewsAdapter
 lateinit var recyler:RecyclerView
+var articles= mutableListOf<Articles>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyler=findViewById(R.id.myRecycler)
+        adapter = NewsAdapter(this@MainActivity, articles)
+        recyler.adapter=adapter
+//        recyler.layoutManager=LinearLayoutManager(this@MainActivity)
+        val stackLayoutManager=StackLayoutManager(StackLayoutManager.ScrollOrientation.BOTTOM_TO_TOP)
+        stackLayoutManager.setPagerMode(true)
+        stackLayoutManager.setPagerFlingVelocity(3000)
+        stackLayoutManager.setItemChangedListener(object :StackLayoutManager.ItemChangedListener{
+            override fun onItemChanged(position: Int) {
+                container.setBackgroundColor(Color.parseColor(ColorPicker.getColor()))
+            }
+        })
+        recyler.layoutManager=stackLayoutManager
 
         getNews()
-
     }
 
     private fun getNews() {
-        val  news =newsInstance.getHeadLines("ae",1)
+        val  news =newsInstance.getHeadLines("in",1)
         news.enqueue(object:Callback<News>{
             override fun onResponse(call: Call<News>, response: Response<News>) {
             val news=response.body()
 
                 if (news != null) {
-                     adapter = NewsAdapter(this@MainActivity, news.articles)
-                    recyler.adapter=adapter
-                    recyler.layoutManager=LinearLayoutManager(this@MainActivity)
+                    articles.addAll(news.articles)
+                    adapter.notifyDataSetChanged()
                 }
             }
 
             override fun onFailure(call: Call<News>, t: Throwable) {
                 Toast.makeText(application,"Error in fetching the news ",Toast.LENGTH_LONG).show()
-                
+
             }
         })
     }
